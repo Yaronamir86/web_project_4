a87d441abe59320326ffbabbd9d4521fdb65a970
yaron amir <yaronamir86@gmail.com>
2022-04-10 00:10:51 +0300

    first changes pro 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,40 @@
-let popUpOpen = document.querySelector(".popup");
+const popUpOpen = document.querySelector(".popup");
 
-let popUpForm = document.querySelector(".popup__form");
+const popUpForm = document.querySelector(".popup__form");
 
-let editBtn = document.querySelector(".profile__edit-btn");
+const editBtn = document.querySelector(".profile__edit-btn");
 
-let addBtn = document.querySelector(".profile__add-btn");
+const addBtn = document.querySelector(".profile__add-btn");
 
-let inputName = document.querySelector(".popup__text_type_name");
+const inputName = document.querySelector(".popup__text_type_name");
 
-let inputAboutMe = document.querySelector(".popup__text_type_about-me");
+const inputAboutMe = document.querySelector(".popup__text_type_about-me");
 
-let likeBtn = document.querySelector(".element__like-btn");
+const likeBtn = document.querySelector(".element__like-btn");
 
-let profileName = document.querySelector(".profile__name");
+const profileName = document.querySelector(".profile__name");
 
-let profileAboutMe = document.querySelector(".profile__about-me");
+const profileAboutMe = document.querySelector(".profile__about-me");
 
-let saveBtn = document.querySelector(".popup__save-btn");
+const saveBtn = document.querySelector(".popup__save-btn");
 
-let closeBtn = document.querySelector(".popup__close-btn");
+const popupCloseBtn = document.querySelector(".popup__close-btn");
+
+const placeForm = document.querySelector(".place-modal__form");
+
+const placeModalOpen = document.querySelector(".place-modal");
+
+const placeCloseBtn = document.querySelector(".place-modal__close-btn");
+
+const createBtn = document.querySelector(".place-modal__create-btn");
+
+const placeTitle = document.querySelector(".place-modal__title");
+
+const placeImage = document.querySelector(".place-modal__image");
+
+
+
+//////////edit-profile-functions////////////////////////
 
 function openPopup() {
   popUpOpen.classList.add("popup_opened");
@@ -36,7 +52,37 @@ function formsubmit(event) {
   profileAboutMe.textContent = inputAboutMe.value;
 
   closePopup();
+  }
+
+  
+
+  popUpForm.addEventListener("submit", formsubmit);
+  popupCloseBtn.addEventListener("click", closePopup);
+  editBtn.addEventListener("click", openPopup);
+  
+
+  ////////////add-image-functions//////////////////////
+
+
+  function openPlaceModal() {
+    placeModalOpen.classList.add("place-modal_opened");
+    inputName.value = profileName.textContent;
+    inputAboutMe.value = profileAboutMe.textContent; 
+  }
+  
+  function closePlaceModal() {
+    placeModalOpen.classList.remove("place-modal_opened");
+  }
+  
+  function formsubmit(event) {
+    event.preventDefault();
+    profileName.textContent = inputName.value;
+    profileAboutMe.textContent = inputAboutMe.value;
+  
+    
 }
-popUpForm.addEventListener("submit", formsubmit);
-closeBtn.addEventListener("click", closePopup);
-editBtn.addEventListener("click", openPopup);
+
+
+addBtn.addEventListener("click", openPlaceModal);
+placeCloseBtn.addEventListener("click",closePlaceModal);
+
