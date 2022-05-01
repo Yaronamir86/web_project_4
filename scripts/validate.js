f488afa72724b4f29ea5dd8551b14074b3fc2168
yaron amir <yaronamir86@gmail.com>
2022-05-01 12:36:51 +0300

    more checking on js

--- scripts/validate.js
+++ scripts/validate.js
@@ -75,8 +75,8 @@ const showInputError = (formElement, inputElement, configurations) => {
   const configurations = {
   formSelector: ".form",
   inputSelector: ".form__input",
-  submitButtonSelector: ".form__save-btn",
-  inactiveButtonClass: "form__save-btn_disabled",
+  submitButtonSelector: ".form__button",
+  inactiveButtonClass: "form__button_disabled",
   inputErrorClass: "form__input_type_error",
   errorClass: "form__error_visible",
   };
